7e87afde6e97fc1c1dc457819ec0d26997d8e207
Ajay Jogdand <ajayjogdand374@gmail.com>
2021-11-26 12:10:32 +0530

    ecommerce integration

--- ecommerce_integrations/shopify/product.py
+++ ecommerce_integrations/shopify/product.py
@@ -138,7 +138,7 @@ class ShopifyProduct:
 		variant_id = product_dict.get("variant_id", "")  # shopify variant_id if has variants
 		sku = item_dict["sku"]
 
-		if not _match_sku_and_link_item(
+		if _match_sku_and_link_item(
 			item_dict, integration_item_code, variant_id, variant_of=variant_of, has_variant=has_variant
 		):
 			ecommerce_item.create_ecommerce_item(
