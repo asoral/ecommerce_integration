8f4f72c17f29b5d711807041cb93cb6e1299952c
Ajay Jogdand <ajayjogdand374@gmail.com>
2021-11-30 15:43:54 +0530

    shopify

--- ecommerce_integrations/shopify/shopify_variant_item.py
+++ ecommerce_integrations/shopify/shopify_variant_item.py
@@ -27,6 +27,48 @@ def update_variant(item_code):
 			"integration_item_code",
 		)
     key=frappe.db.get_single_value('Shopify Setting', 'api_key')
+    shopify_url=frappe.db.get_single_value('Shopify Setting', 'shopify_url')
+    docSettings = frappe.get_single("Shopify Setting")
+    password = docSettings.get_password('password')
+
+    # url="https://"+key+":"+password+"@farmley-dry-fruit.myshopify.com/admin/api/2021-10/products/"+product_id+".json"
+
+    url = "https://"+key+":"+password+"@"+shopify_url+"/admin/api/2021-10/products/"+product_id+"/variants.json"
+    # url="https://{{api_key}}:{{api_password}}@{{store_name}}.myshopify.com/admin/api/{{api_version}}/variants/{{variant_id}}.json"
+
+    doc=frappe.db.get_all("Item",{"variant_of":item_code},["name"])
+    for i in doc:
+        sdoc=frappe.get_doc("Item",i.name)
+        for i in sdoc.attributes:
+            if i.attribute=="Size":
+                payload = json.dumps({
+                # "options":{
+                #     "name":"Size",
+                #     "position":1,
+                #     "values":i.attribute_value
+                    
+                # }
+                "variant": {
+                    "option1":i.attribute_value,
+                    "sku":sdoc.name
+                }
+                })
+                headers = {
+                'Content-Type': 'application/json'
+                }
+
+                response = requests.request("POST", url, headers=headers, data=payload)
+
+                print(response.text)
+
+@frappe.whitelist()
+def update_product(item_code):
+    product_id = frappe.db.get_value(
+			"Ecommerce Item",
+			{"erpnext_item_code": item_code, "integration": MODULE_NAME},
+			"integration_item_code",
+		)
+    key=frappe.db.get_single_value('Shopify Setting', 'api_key')
     docSettings = frappe.get_single("Shopify Setting")
     password = docSettings.get_password('password')
 
@@ -57,6 +99,3 @@ def update_variant(item_code):
                 }
 
                 response = requests.request("POST", url, headers=headers, data=payload)
-
-                print(response.text)
-
