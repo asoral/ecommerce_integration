8aad024c22486f475a48621139ed3bacc362988b
Ajay Jogdand <ajayjogdand374@gmail.com>
2021-12-02 19:18:40 +0530

    changes

--- ecommerce_integrations/shopify/connection.py
+++ ecommerce_integrations/shopify/connection.py
@@ -30,7 +30,7 @@ def temp_shopify_session(func):
 		setting = frappe.get_doc(SETTING_DOCTYPE)
 		if setting.is_enabled():
 			auth_details = (setting.shopify_url, API_VERSION, setting.get_password("password"))
-
+			print("&&&&&&&&888888888",auth_details)
 			with Session.temp(*auth_details):
 				print("&&&&&&&&&&&&&&",func(*args, **kwargs))
 				return func(*args, **kwargs)
