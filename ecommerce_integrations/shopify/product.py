df6ee6617a19eae311dbb7929cb1540859702d6f
Ajay Jogdand <ajayjogdand374@gmail.com>
2021-12-02 19:04:01 +0530

    changes

--- ecommerce_integrations/shopify/product.py
+++ ecommerce_integrations/shopify/product.py
@@ -399,7 +399,7 @@ def upload_erpnext_item(doc, method=None):
 
 @frappe.whitelist()
 @temp_shopify_session
-def update_item_shopify(doc,method=None):
+def update_item_shopify(doc):
 	item = frappe.get_doc("Item",doc)
 	product_id = frappe.db.get_value(
 			"Ecommerce Item",
